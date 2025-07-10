b7568e58aca104214d5ebd36a272381a28abd578
Daniel Gonzalez <146906675+DANIElPEZ@users.noreply.github.com>
2025-07-10 18:58:18 -0500

    Delete delete directory

--- delete/api/date.js
+++
@@ -1,9 +0,0 @@
-const moment = require("moment-timezone");
-
-export default function handler(req, res) {
-  const now = moment().tz("America/Bogota");
-  const day = now.format("dddd");
-  const hour = now.hour();
-
-  res.status(200).json({ day, hour });
-}
