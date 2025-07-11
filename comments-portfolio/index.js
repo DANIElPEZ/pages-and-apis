cf6ba7b699721a6267b8882561ed4a58e1b3f49f
Daniel Gonzalez <146906675+DANIElPEZ@users.noreply.github.com>
2025-07-10 23:07:41 -0500

    Create index.js

---
+++ comments-portfolio/index.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+dotenv.config();
+
+const app = express();
+const uri = process.env.uri;
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/comments', async (req, res) => {
+  try {
+    const db = client.db('dnvdev').collection('comments');
+    const result = await db.find({}).toArray();
+    res.json(result);
+  } catch (err) {
+    res.status(400).send('Error al obtener los datos');
+  }
+});
+
+app.post('/comments', async (req,res)=>{
+  try {
+    const sanitizedInput = sanitizeInput(req.body.name, req.body.comment);
+    const name = sanitizedInput.name?.trim();
+    const comment = sanitizedInput.comment?.trim();
+    if (!name || !comment || name.length < 15 || comment.length < 10) return res.status(400).send('Datos no válidos');
+
+    const db = client.db('dnvdev').collection('comments');
+    await db.insertOne({
+      name: sanitizedInput.name,
+      comment: sanitizedInput.comment
+    });
+    res.status(201).send('Comentario insertado correctamente');
+  } catch (error) {
+    res.status(400).send('Error al insertar los datos');
+  }
+});
+
+app.listen(3000, async() => {
+  try {
+    await client.connect(); 
+  } catch (error) {
+    console.log('error al conectarse a la base de datos');
+  }
+  console.log('Servidor escuchando en http://localhost:3000');
+});
+
+function sanitizeInput(name, comment) {
+     const sanitizeName = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+     const sanitizeComment = /^[\p{L}\p{Emoji_Presentation}\p{Emoji}\s]+$/u;
+
+     const isValidName = sanitizeName.test(name.trim());
+     const isValidComment = sanitizeComment.test(comment.trim());
+
+     return {
+          name: isValidName,
+          comment: isValidComment
+     };
+}
